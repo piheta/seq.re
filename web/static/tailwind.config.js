5dd2a8e69008dfd12ca4237a340beafaa2848b0e
piheta <m@picheta.dev>
2025-12-08 20:13:46 +0100

    Replace style tags with tailwind styles

---
+++ web/static/tailwind.config.js
@@ -0,0 +1,87 @@
+tailwind.config = {
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        'dr-bg': {
+          DEFAULT: '#ffffff',
+          dark: '#111415'
+        },
+        'dr-text': {
+          DEFAULT: '#000000',
+          dark: '#ffffff'
+        },
+        'dr-bg-gray': {
+          DEFAULT: '#f5f5f5',
+          dark: '#1a1d1e'
+        },
+        'dr-bg-cream': {
+          DEFAULT: '#faedda',
+          dark: '#512f00'
+        },
+        'dr-border': {
+          DEFAULT: '#e5e7eb',
+          dark: '#3e4548'
+        },
+        'dr-text-muted': {
+          DEFAULT: '#9ca3af',
+          dark: '#f3ead9'
+        },
+        'dr-bg-subtle': {
+          DEFAULT: '#f3f4f6',
+          dark: '#1a1d20'
+        },
+        'dr-bg-page': {
+          DEFAULT: '#f9fafb',
+          dark: '#15181a'
+        },
+        'dr-blue': {
+          DEFAULT: '#2563eb',
+          light: '#41c3ff',
+          dark: '#0551f7',
+          darker: '#0f4af0'
+        },
+        'dr-text-gray': {
+          DEFAULT: '#4b5563',
+          light: '#f5ebdc',
+          dark: '#374151',
+          darker: '#111827'
+        },
+        'dr-text-heading': {
+          DEFAULT: '#111827',
+          dark: '#ffffff'
+        },
+        'dr-green': {
+          DEFAULT: '#16a34a',
+          dark: '#7cffc9'
+        },
+        'dr-indigo': {
+          DEFAULT: '#4f46e5',
+          dark: '#66b2ff'
+        },
+        'dr-orange': {
+          DEFAULT: '#ea580c',
+          dark: '#ff942d'
+        },
+        'dr-purple': {
+          DEFAULT: '#9333ea',
+          dark: '#d550ff'
+        },
+        'dr-border-secondary': {
+          DEFAULT: '#d1d5db',
+          dark: '#474e51'
+        },
+        'dr-text-body': {
+          DEFAULT: '#374151',
+          dark: '#fffef3'
+        },
+        'dr-bg-blue': {
+          DEFAULT: '#2563eb',
+          dark: '#0551f7',
+          hover: '#1d4ed8',
+          'hover-dark': '#0f4af0'
+        }
+      }
+    }
+  }
+}
